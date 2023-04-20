1c2bafa5c76759bcef90e714184f4f7ce4afdd68
Koen Van Hul <koen.van.hul@gmail.com>
2023-04-20 21:36:30 +0200

    Changed Main Component from tsx to jsx

--- assets/js/MainComponent.jsx
+++ assets/js/MainComponent.jsx
@@ -1,19 +1,15 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
+import {Button} from 'react-bootstrap';
 
-interface propMapping {
-    controllerName: string
-}
-
-const MainComponent = ({controllerName}: propMapping) => {
-
+export default class extends Component {
+  render () {
     return (
-        <Fragment>
+      <Fragment>
             <h1>Here starts your app</h1>
             <p>This is called from this controller: {controllerName}</p>
             <p>Please read the readme.md file in root</p>
             <p>Change the colors in the ./assets/styles/app.scss </p>
-        </Fragment>
-    );
+      </Fragment>
+    );      
+  }
 }
-
-export default MainComponent
