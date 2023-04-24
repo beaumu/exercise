48dd7dde3cd2073ef9ab32a4c3bbd8d672a3fb44
Koen Van Hul <koen.van.hul@gmail.com>
2023-04-24 22:57:17 +0200

    Add simple security for admin section using MemoryUserProvider

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,8 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function admin(): Response
+    public function index(): Response
     {
-        return $this->render('default/admin.html.twig');
+        return $this->render('admin/index.html.twig');
     }
 }
